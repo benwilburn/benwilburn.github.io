5cb66056c069acdbe8956e70bf385fd0f5f53b67
Benjamin Wilburn <bwilburnstrength@gmail.com>
2016-08-10 16:46:31 -0500

    set up angular controller and factory

--- main_controller.js
+++ main_controller.js
@@ -1,4 +1,10 @@
 angular.module('personal-site')
-.controller('website-ctrl', function(MainFactory){
-  console.log('hit');
-})
+  .controller('website-ctrl', function($scope, MainFactory){
+    $scope.app_name = "Ben's Portfolio"
+    MainFactory.getLinks().then(res => {
+      console.log("wat", res)
+      $scope.data = res
+    })
+
+    
+  })
--- main_factory.js
+++ main_factory.js
@@ -1,4 +1,11 @@
 angular.module('personal-site')
-.factory('MainFactory', function($http, $scope) {
-
-})
+  .factory('MainFactory', function($http) {
+    return {
+      getLinks: function () {
+        return $http.get('websites.json').then((res) => {
+          var info = res.data
+          return info
+        })
+      }
+    }
+  })
